96f527e9894de79aa6979df248466158255ce11d
Bogdan Semeniuk <000sbv000@gmail.com>
2026-02-16 20:43:18 +0200

    HTTPClient refactoring

--- ComicsWiki/Core/Networking/HTTPClient.swift
+++ ComicsWiki/Core/Networking/HTTPClient.swift
@@ -8,22 +8,24 @@
 import Foundation
 
 protocol HTTPClient {
-    func send<T: Decodable>(_ endpoint: Endpoint, as type: T.Type) async throws -> T
+    func send<T: Decodable & Sendable>(_ endpoint: Endpoint, as type: T.Type) async throws -> T
 }
 
 struct LiveHTTPClient: HTTPClient {
-    var session: URLSession
-    var decoder: JSONDecoder
+    private let session: URLSession
+    private let decoder: JSONDecoder
 
     init(
-        session: URLSession = .shared, decoder: JSONDecoder = .init()) {
+        session: URLSession = .shared,
+        decoder: JSONDecoder = .init()
+    ) {
         self.session = session
         self.decoder = decoder
     }
 
-    func send<T: Decodable>(_ endpoint: Endpoint, as type: T.Type) async throws -> T {
+    func send<T: Decodable & Sendable>(_ endpoint: Endpoint, as type: T.Type) async throws -> T {
         guard let request = URLRequest(endpoint: endpoint) else { throw NetworkError.badRequest }
-
+        
         do {
             let (data, _) = try await session.data(for: request)
             do { return try decoder.decode(T.self, from: data) }
